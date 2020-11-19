befb8257d887ee5bac7d04d9a80d1836a17392dd
zhengyuanfeng <yuanfeng.zheng@bioknow.net>
2020-11-19 18:16:38 +0800

    redis的maven依赖

---
+++ src/main/java/com/zzl/redis/TestRedis.java
@@ -0,0 +1,18 @@
+package com.zzl.redis;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+public class TestRedis {
+	
+	public static void main(String[] args) {
+		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+		/**
+		 * String
+		 * List
+		 * Hash
+		 * Set
+		 * Zset
+		 */
+//		stringRedisTemplate.opsForHash()
+	}
+}
