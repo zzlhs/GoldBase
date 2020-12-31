0bfa87029754b6d85b625391afce726abe77b5fc
zhengyuanfeng <yuanfeng.zheng@bioknow.net>
2020-12-31 14:29:45 +0800

    1 定时任务的多线程使用

---
+++ src/main/java/com/zzl/es/config/ScheduleConfig.java
@@ -0,0 +1,45 @@
+package com.zzl.es.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig implements AsyncConfigurer,SchedulingConfigurer {
+
+	@Override
+	public Executor getAsyncExecutor() {
+		// TODO Auto-generated method stub
+//		return AsyncConfigurer.super.getAsyncExecutor();
+		return taskExecutor();
+	}
+	
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+		  ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		  //当前线程数
+		  scheduler.setPoolSize(20);
+		  //定时任务前缀
+		  scheduler.setThreadNamePrefix("myselfThreadPool");
+		  //该方法用来设置线程池中 任务的等待时间，如果超过这个时间还没有销毁就 强制销毁，以确保应用最后能够被关闭，而不是阻塞住
+		  scheduler.setAwaitTerminationSeconds(20);
+		  // 该方法用来设置 线程池关闭 的时候 等待 所有任务都完成后，再继续 销毁 其他的 Bean，这样这些 异步任务 的 销毁 就会先于 数据库连接池对象 的销毁
+		  scheduler.setWaitForTasksToCompleteOnShutdown(true);
+    	
+    	
+        return scheduler; //指定线程池大小
+    }
+}
+
---
+++ src/main/java/com/zzl/es/task/ScheduledTask.java
@@ -0,0 +1,47 @@
+package com.zzl.es.task;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableAsync
+public class ScheduledTask {
+	
+	@Scheduled(cron = "* * * * * *")
+	@Async
+	public void task1() {
+		System.out.println("task1 -> " + Thread.currentThread().getName());
+		try {
+			System.out.println("task1-1 -> ");
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("task1-2 -> ");
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("task1-3 -> ");
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("task1-4 -> ");
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("task1-5 -> ");
+			TimeUnit.SECONDS.sleep(10);
+			System.out.println("task1-6 -> ");
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Scheduled(cron = "* * * * * *")
+	public void task2() {
+		System.out.println("task2 -> " + Thread.currentThread().getName());
+
+	}
+	
+	@Scheduled(cron = "* * * * * *")
+	public void task3() {
+		System.out.println("task3 -> " + Thread.currentThread().getName());
+
+	}
+}
