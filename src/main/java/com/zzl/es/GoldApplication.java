0959dce084342a04ba67ad221b013b1de700dc7d
zhengyuanfeng <yuanfeng.zheng@bioknow.net>
2021-03-09 10:36:57 +0800

    1

--- src/main/java/com/zzl/es/GoldApplication.java
+++ src/main/java/com/zzl/es/GoldApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoldApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GoldApplication.class, args);
 	}
 }
